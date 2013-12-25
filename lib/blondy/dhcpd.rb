1c17da8b04a1374a513f882267aef3f110fe218f
Pavel Novitskiy <altusensix@gmail.com>
2013-12-25 18:18:16 +0400

    fix class name

--- lib/blondy/dhcpd.rb
+++ lib/blondy/dhcpd.rb
@@ -4,7 +4,7 @@ require_relative 'dhcpd/server'
 module Blondy
   module DHCPD
     EM.run do
-      EM.open_datagram_socket('0.0.0.0', 67, Dispatcher)
+      EM.open_datagram_socket('0.0.0.0', 67, Server)
     end
   end
 end
