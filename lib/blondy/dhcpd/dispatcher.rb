fac765c14b5c82dcaa226ec175492e06601a56f8
Pavel Novitskiy <altusensix@gmail.com>
2013-12-25 14:54:14 +0400

    dispatcher now is deferrable

--- lib/blondy/dhcpd/dispatcher.rb
+++ lib/blondy/dhcpd/dispatcher.rb
@@ -3,7 +3,8 @@ require 'net-dhcp'
 module Blondy
   module DHCPD
     class Dispatcher
-      def self.dispatch(data)
+      include EM::Deferrable
+      def dispatch(data, ip, port)
 	
       end
     end
--- lib/blondy/dhcpd/server.rb
+++ lib/blondy/dhcpd/server.rb
@@ -1,11 +1,16 @@
 require 'net-dhcp'
 require 'eventmachine'
+require 'socket'
 
 module Blondy
   module DHCPD
     class Server < EM::Connection
       def receive_data(data)
-	Dispatcher.dispatch(data)
+	ip, port = Socket.unpack_sockaddr_in(get_peername)
+	dispatcher = Dispatcher.new
+	dispatcher.dispatch(data, ip, port)
+	dispatcher.callback { |reply| send_datagram(reply.data, reply.ipaddr, reply.port) }
+	dispatcher.errback { |message| puts message }
       end
     end
   end
