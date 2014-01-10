af4227cc08f83a9447c46eac08b74115031098d1
Pavel Novitskiy <altusensix@gmail.com>
2014-01-10 14:49:02 +0400

    add file outputter for logger

--- lib/blondy/dhcpd/logger.rb
+++ lib/blondy/dhcpd/logger.rb
@@ -4,5 +4,11 @@ module Blondy
   module DHCPD
     Logger = Log4r::Logger.new 'ruby-dhcpd'
     Logger.outputters << Log4r::Outputter.stdout
+    format = Log4r::PatternFormatter.new(:pattern => "[%l] [%d] %m")
+    if /^\// =~ Blondy::DHCPD::CONFIG['log_path']
+      log_path = Blondy::DHCPD::CONFIG['log_path'].gsub(/\/*$/,'')
+      Logger.outputters << Log4r::FileOutputter.new('blondy-dhcpd.log', filename: "#{log_path}/blondy-dhcpd.log" , formatter: format)
+    end
+    Logger.level = ((1..7).include?(Blondy::DHCPD::CONFIG['log_level']) ? Blondy::DHCPD::CONFIG['log_level'] : 1)
   end
 end
