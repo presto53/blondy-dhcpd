bdf3d312a854f5997b4d81de49fae542550ee393
Pavel Novitskiy <altusensix@gmail.com>
2014-01-10 17:30:56 +0400

    move logger to main file

--- lib/blondy/dhcpd.rb
+++ lib/blondy/dhcpd.rb
@@ -1,40 +1,57 @@
 require 'eventmachine'
 require 'yaml'
+require 'log4r'
 require_relative 'dhcpd/server'
-require_relative 'dhcpd/logger'
 
-default_config = '/etc/blondy/dhcpd.yml'
+module Blondy
+  module DHCPD
+    default_config = '/etc/blondy/dhcpd.yml'
 
-begin
-  Blondy::DHCPD::CONFIG = YAML::load(File.open(ENV['BLONDY_CONFIGPATH'] || default_config))
-rescue
-  STDERR.puts "No config file. \nPlease check that #{default_config} exist or BLONDY_CONFIGPATH is set."
-  exit 1
-end
+    begin
+      CONFIG = YAML::load(File.open(ENV['BLONDY_CONFIGPATH'] || default_config))
+    rescue
+      STDERR.puts "No config file. \nPlease check that #{default_config} exist or BLONDY_CONFIGPATH is set."
+      exit 1
+    end
 
-if /^\// =~ Blondy::DHCPD::CONFIG['pid_path']
-  pidf = "#{Blondy::DHCPD::CONFIG['pid_path'].gsub(/\/*$/,'')}/blondy-dhcpd.pid"
-  running_pid = File.open(pidf, 'r').read.chomp rescue nil
-  running_pgid = Process.getpgid(running_pid.to_i) rescue nil if running_pid
-  if running_pgid
-    STDERR.puts 'Daemon already running.'
-    exit 1
+    Logger = Log4r::Logger.new 'ruby-dhcpd'
+    Logger.outputters << Log4r::Outputter.stdout
+    format = Log4r::PatternFormatter.new(:pattern => "[%l] [%d] %m")
+    begin
+      if /^\// =~ CONFIG['log_path']
+	log_path = CONFIG['log_path'].gsub(/\/*$/,'')
+	Logger.outputters << Log4r::FileOutputter.new('blondy-dhcpd.log', filename: "#{log_path}/blondy-dhcpd.log" , formatter: format)
+      end
+      Logger.level = ((1..7).include?(CONFIG['log_level']) ? CONFIG['log_level'] : 1)
+    rescue
+      STDERR.puts 'No config loaded or log_path missed.'
+    end
+
+    if /^\// =~ CONFIG['pid_path']
+      pidf = "#{CONFIG['pid_path'].gsub(/\/*$/,'')}/blondy-dhcpd.pid"
+      running_pid = File.open(pidf, 'r').read.chomp rescue nil
+      running_pgid = Process.getpgid(running_pid.to_i) rescue nil if running_pid
+      if running_pgid
+	STDERR.puts 'Daemon already running.'
+	exit 1
+      end
+    else
+      STDERR.puts 'PID path is wrong or not set.'
+      Logger.error 'PID path is wrong or not set.'
+      exit 1
+    end
+
+    Process.daemon
+    File.write(pidf, "#{Process.pid}\n")
+
+    Signal.trap("TERM") do
+      EM.stop
+      File.delete(pidf) if File.exists?(pidf)
+      exit 0
+    end
+
+    EM.run do
+      EM.open_datagram_socket('0.0.0.0', 67, Server)
+    end
   end
-else
-  STDERR.puts 'PID path is wrong or not set.'
-  Blondy::DHCPD::Logger.error 'PID path is wrong or not set.'
-  exit 1
-end
-
-Process.daemon
-File.write(pidf, "#{Process.pid}\n")
-
-Signal.trap("TERM") do
-  EM.stop
-  File.delete(pidf) if File.exists?(pidf)
-  exit 0
-end
-
-EM.run do
-  EM.open_datagram_socket('0.0.0.0', 67, Blondy::DHCPD::Server)
 end
