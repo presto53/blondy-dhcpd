82d72e97493542cd35aa61a8c5607bf67c15269b
Pavel Novitskiy <altusensix@gmail.com>
2014-01-17 12:20:52 +0400

    add commandline options

--- lib/blondy/dhcpd.rb
+++ lib/blondy/dhcpd.rb
@@ -1,3 +1,6 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
 require 'eventmachine'
 require 'yaml'
 require 'log4r'
@@ -5,9 +8,22 @@ require_relative 'dhcpd/server'
 
 module Blondy
   module DHCPD
+    # Set default config
     default_config = '/etc/blondy/dhcpd.yml'
     config_file = default_config
 
+    # Read command line options
+    @options = Hash.new
+    OptionParser.new do |opts|
+      opts.banner = 'Usage: dhcpd.rb [options]'
+      opts.on('-d', '--debug', 'Run foreground for debug') { @options[:debug] = true }
+      opts.on_tail('-h', '--help', 'Show this message') do
+	puts opts
+	exit 0
+      end
+    end.parse!
+
+    # Load config from file
     begin
       config_file = "#{ENV['BLONDY_CONFIGPATH']}/dhcpd.yml" if ENV['BLONDY_CONFIGPATH']
       CONFIG = YAML::load(File.open(config_file))
@@ -16,6 +32,7 @@ module Blondy
       exit 1
     end
 
+    # Set logger
     Logger = Log4r::Logger.new 'ruby-dhcpd'
     Logger.outputters << Log4r::Outputter.stdout
     format = Log4r::PatternFormatter.new(:pattern => "[%l] [%d] %m")
@@ -29,6 +46,7 @@ module Blondy
       STDERR.puts 'No config loaded or log_path missed.'
     end
 
+    # Check if daemon already running
     if /^\// =~ CONFIG['pid_path']
       @pidf = "#{CONFIG['pid_path'].gsub(/\/*$/,'')}/blondy-dhcpd.pid"
       running_pid = File.open(@pidf, 'r').read.chomp rescue nil
@@ -43,24 +61,33 @@ module Blondy
       exit 1
     end
 
-    #    Process.daemon
+    # Daemonize
+    Process.daemon unless @options[:debug]
+
+    # Write PID
     File.write(@pidf, "#{Process.pid}\n")
     Logger.info "Starting dhcpd with pid #{Process.pid}"
 
+    # Signal handlers
     @signals = Array.new
     class << self
-      def shutdown
+      def shutdown(exit_code)
 	Logger.info "Shutdown server..."
 	EM.stop if EM.reactor_running?
 	File.delete(@pidf) if File.exists?(@pidf)
+	exit exit_code
       end
       def term_handler
-	  Logger.info "Server received TERM signal."
-	  shutdown
-	  exit 0
+	Logger.info "Server received TERM signal."
+	shutdown(0)
+      end
+      def int_handler
+	Logger.info "Server received INT signal."
+	shutdown(0)
       end
     end
 
+    # Start server
     if Process.uid != 0
       Logger.error 'Failed to start server. Server should be started by root.'
       shutdown
@@ -68,7 +95,12 @@ module Blondy
     else
       EM.run do
 	Signal.trap('TERM') { @signals << :term }
-	EM.add_periodic_timer(1) { term_handler if @signals.include?(:term) }
+	Signal.trap('INT') { @signals << :int }
+	# Check for signals periodically
+	EM.add_periodic_timer(1) do
+	  term_handler if @signals.include?(:term)
+	  int_handler if @signals.include?(:int)
+	end
 	EM.open_datagram_socket('0.0.0.0', 67, Server)
       end
     end
