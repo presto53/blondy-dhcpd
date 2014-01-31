56e9503ab4721935ba1e2d619c4ab414c0dc446b
Pavel Novitskiy <altusensix@gmail.com>
2014-01-31 17:50:40 +0400

    dispatcher refactoring

--- lib/blondy/dhcpd/dispatcher.rb
+++ lib/blondy/dhcpd/dispatcher.rb
@@ -9,39 +9,36 @@ module Blondy
 	def dispatch(data, ip, port)
 	  @data = DHCP::Message.from_udp_payload(data) rescue raise(IncorrectMessage, 'Incorrect message received.')
 	  raise(IncorrectMessage, 'Incorrect message received.') unless @data
-	  DHCP::Message.class_eval {attr_accessor :hwaddr}
-	  @data.hwaddr = @data.chaddr.take(@data.hlen).map {|x| x.to_s(16).size<2 ? '0'+x.to_s(16) : x.to_s(16)}.join(':')
-	  @reply = OpenStruct.new
-	  msg_class = @data.class.to_s.gsub(/^.*::/, '').downcase
-	  send("#{msg_class}_handler".to_sym)
-	  if @reply.data
-	    @reply.data.xid = @data.xid if @data.xid && @reply.data
-	    @reply
-	  else
-	    false
-	  end
+	  reply
 	end
 
 	private
 
-	def discover_handler
-	  @pool = Pool.query(@data.hwaddr, :discover)
-	  if @pool
-	    @reply.data = DHCP::Offer.new
-	    create_reply
+	def reply
+	  DHCP::Message.class_eval {attr_accessor :hwaddr}
+	  @data.hwaddr = @data.chaddr.take(@data.hlen).map {|x| x.to_s(16).size<2 ? '0'+x.to_s(16) : x.to_s(16)}.join(':')
+	  @reply = OpenStruct.new
+	  msg_class = @data.class.to_s.gsub(/^.*::/, '').downcase
+	  handle(msg_class)
+	end
+
+	def handle(msg_class)
+	  if %w{discover request inform release}.include?(msg_class)
+	    @pool = Pool.query(@data.hwaddr, msg_class.to_sym)
+	    @pool ? send("#{msg_class}_handler".to_sym) : false
 	  else
-	    @reply.data = nil
+	    raise NoMessageHandler, 'No appropriate handler for message.'
 	  end
 	end
 
+	def discover_handler
+	    @reply.data = DHCP::Offer.new
+	    create_reply
+	end
+
 	def request_handler
-	  @pool = Pool.query(@data.hwaddr, :request)
-	  if @pool
 	    @reply.data = DHCP::ACK.new
 	    create_reply
-	  else
-	    @reply.data = nil
-	  end
 	end
 
 	def release_handler
@@ -50,10 +47,21 @@ module Blondy
 
 	def inform_handler
 	  @reply.data = DHCP::ACK.new
+	  create_reply
 	end
 
 	def create_reply
+	  @reply.data.yiaddr = @pool.data.yiaddr
+	  @reply.data.fname = @pool.data.fname
+	  @reply.data.options = @pool.data.options
+	  @reply.data.siaddr = IPAddr.new(Blondy::DHCPD::CONFIG['server_ip']).to_i
+	  @reply.data.xid = @data.xid if @data.xid
 	  @reply.ip = '255.255.255.255'
+	  set_port
+	  @reply
+	end
+
+	def set_port
 	  @reply.port = 68
 	  if @data.giaddr == 0 and @data.ciaddr != 0
 	    @reply.ip = IPAddr.new(@data.ciaddr, family = Socket::AF_INET).to_s
@@ -63,14 +71,6 @@ module Blondy
 	  else
 	    false
 	  end
-	  @reply.data.yiaddr = @pool.data.yiaddr
-	  @reply.data.fname = @pool.data.fname
-	  @reply.data.options = @pool.data.options
-	  @reply.data.siaddr = IPAddr.new(Blondy::DHCPD::CONFIG['server_ip']).to_i
-	end
-
-	def method_missing(*args)
-	  raise NoMessageHandler, 'No appropriate handler for message.'
 	end
       end
     end
